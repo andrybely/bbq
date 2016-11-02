2c60bd7228edf85cc1e3730cec8a26475505a2a5
bely <bely10@ya.ru>
2016-11-02 20:41:08 +0300

    add event delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -85,6 +85,6 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:title, :address, :datetime, :description, :pincode)
+    params.require(:event).permit(:title, :address, :datetime, :description, :pincode, :destroy)
   end
 end
